a8932d1d43a4a6b7808d26d02d35822464c86ce5
iangioffre <igioffre@zagmail.gonzaga.edu>
2019-01-24 18:27:46 -0800

    translated all c++ code

--- Assignment 1/src/yahtzee.java
+++ Assignment 1/src/yahtzee.java
@@ -1,120 +1,132 @@
-/***************
-Homework #1
-Due Date: Jan 29, 2018
-Names: Cole Donovan
-********************/
+/****************************************
+	Homework #1
+	Due Date: 01-29-2019
+	Names: Ian Gioffre and Cole Donovan
+****************************************/
+
+import java.util.Scanner;
 
 public class yahtzee
 {
 	public static void main(String[] args)
 	{
-	    final int DICE_IN_PLAY = 5;
-	    int hand[DICE_IN_PLAY];
-	    char playAgain = 'y';
-	
-	while (playAgain == 'y')
-	{
-	    String keep = "nnnnn"; //setup to roll all dice in the first roll
-	    int turn = 1;
-	    while (turn < 4 && keep != "yyyyy")
-	    {
-	        //roll dice not kept
-	        for (int dieNumber = 0; dieNumber < DICE_IN_PLAY; dieNumber++)
-	        {
-	            if (keep.charAt(dieNumber) != 'y')
-	                hand[dieNumber] = rollDie();
-	        }
-	        //output roll
-	        System.out.println("Your roll was: ");
-	        for (int dieNumber = 0; dieNumber < DICE_IN_PLAY; dieNumber++)
-	        {
-	            System.out.println(hand[dieNumber] + " ");
-	        }
-	        System.out.println();
-	        //if not the last roll of the hand prompt the user for dice to keep
-	        if (turn < 3)
-	        {
-	            System.out.println("enter dice to keep (y or n) ");
-	            cin >> keep;
-	        }
-	        turn++;
-	    }
-	    //start scoring
-	    //hand need to be sorted to check for straights
-	
-	    sortArray(hand, DICE_IN_PLAY);
-	    cout << "Here is your sorted hand : ";
-	    for (int dieNumber = 0; dieNumber < DICE_IN_PLAY; dieNumber++)
-	        {
-	            cout << hand[dieNumber] << " ";
-	        }
-	        cout << endl;
-	    //upper scorecard
-	    for (int dieValue = 1; dieValue <=6; dieValue++)
-	    {
-	        int currentCount = 0;
-	        for (int diePosition = 0; diePosition < 5; diePosition++)
-	        {
-	            if (hand[diePosition] == dieValue)
-	                currentCount++;
-	        }
-	        cout << "Score " << dieValue * currentCount << " on the ";
-	        cout << dieValue << " line" << endl;
-	    }
-	    //lower scorecard
-	    if (maxOfAKindFound(hand) >= 3)
-	    {
-	        cout << "Score " << totalAllDice(hand) << " on the ";
-	        cout << "3 of a Kind line" << endl;
-	    }
-	    else cout << "Score 0 on the 3 of a Kind line" << endl;
-	
-	    if (maxOfAKindFound(hand) >= 4)
-	    {
-	        cout << "Score " << totalAllDice(hand) << " on the ";
-	        cout << "4 of a Kind line" << endl;
-	    }
-	    else cout << "Score 0 on the 4 of a Kind line" << endl;
-	
-	    if (fullHouseFound(hand))
-	        cout << "Score 25 on the Full House line" << endl;
-	    else
-	        cout << "Score 0 on the Full House line" << endl;
-	
-	    if (maxStraightFound(hand) >= 4)
-	        cout << "Score 30 on the Small Straight line" << endl;
-	    else
-	        cout << "Score 0 on the Small Straight line" << endl;
-	
-	    if (maxStraightFound(hand) >= 5)
-	        cout << "Score 40 on the Large Straight line" << endl;
-	    else
-	        cout << "Score 0 on the Large Straight line" << endl;
-	
-	    if (maxOfAKindFound(hand) >= 5)
-	        cout << "Score 50 on the Yahtzee line" << endl;
-	    else
-	        cout << "Score 0 on the Yahtzee line" << endl;
-	
-	    cout << "Score " << totalAllDice(hand) << " on the ";
-	    cout << "Chance line" << endl;
-	    cout << "\nEnter 'y' to play again ";
-	    cin >> playAgain;
+		Scanner scnr = new Scanner(System.in);
+		String input;
+		final int DICE_IN_PLAY = 5;
+		int[] hand = new int[DICE_IN_PLAY];
+		char playAgain = 'y';
+
+		while (playAgain == 'y')
+		{
+			String keep = "nnnnn"; //setup to roll all dice in the first roll
+			int turn = 1;
+			
+			while (turn < 4 && keep != "yyyyy")
+			{
+				//roll dice not kept
+				for (int dieNumber = 0; dieNumber < DICE_IN_PLAY; dieNumber++)
+				{
+					if (keep.charAt(dieNumber) != 'y')
+						hand[dieNumber] = rollDie();
+				}
+				
+				//output roll
+				System.out.print("Your roll was: ");
+				for (int dieNumber = 0; dieNumber < DICE_IN_PLAY; dieNumber++)
+				{
+					System.out.print(hand[dieNumber] + " ");
+				}
+				
+				System.out.println();
+				
+				//if not the last roll of the hand prompt the user for dice to keep
+				if (turn < 3)
+				{
+					System.out.print("enter dice to keep (y or n) ");
+					keep = scnr.nextLine();
+				}
+				turn++;
+			}
+			
+			//start scoring
+			//hand need to be sorted to check for straights
+			sortArray(hand, DICE_IN_PLAY);
+			System.out.print("Here is your sorted hand : ");
+			for (int dieNumber = 0; dieNumber < DICE_IN_PLAY; dieNumber++)
+			{
+				System.out.print(hand[dieNumber] + " ");
+			}
+			System.out.println();
+			
+			//upper scorecard
+			for (int dieValue = 1; dieValue <=6; dieValue++)
+			{
+				int currentCount = 0;
+				for (int diePosition = 0; diePosition < 5; diePosition++)
+				{
+					if (hand[diePosition] == dieValue)
+						currentCount++;
+				}
+				System.out.println("Score " + dieValue * currentCount + " on the " + dieValue + " line");
+			}
+			
+			//lower scorecard
+			if (maxOfAKindFound(hand) >= 3)
+			{
+				System.out.println("Score " + totalAllDice(hand) + " on the " + "3 of a Kind line");
+			}
+			else
+				System.out.println("Score 0 on the 3 of a Kind line");
+
+			if (maxOfAKindFound(hand) >= 4)
+			{
+				System.out.println("Score " + totalAllDice(hand) + " on the " + "4 of a Kind line");
+			}
+			else
+				System.out.println("Score 0 on the 4 of a Kind line");
+
+			if (fullHouseFound(hand))
+				System.out.println("Score 25 on the Full House line");
+			else
+				System.out.println("Score 0 on the Full House line");
+
+			if (maxStraightFound(hand) >= 4)
+				System.out.println("Score 30 on the Small Straight line");
+			else
+				System.out.println("Score 0 on the Small Straight line");
+
+			if (maxStraightFound(hand) >= 5)
+				System.out.println("Score 40 on the Large Straight line");
+			else
+				System.out.println("Score 0 on the Large Straight line");
+
+			if (maxOfAKindFound(hand) >= 5)
+				System.out.println("Score 50 on the Yahtzee line");
+			else
+				System.out.println("Score 0 on the Yahtzee line");
+
+			System.out.println("Score " + totalAllDice(hand) + " on the " + "Chance line");
+			System.out.print("\nEnter 'y' to play again ");
+			input = scnr.nextLine();
+			playAgain = input.charAt(0);
+		}
+		
+		scnr.close();
 	}
-	    return 0;
-	}
-	public static int rollDie()
+	
 	//this function simulates the rolling of a single die
+	public static int rollDie()
 	{
-	    int roll = rand() % 6 + 1;
-	    return roll;
+		return (int) ( ( Math.random() * 10 ) % 6 + 1 );
 	}
-	int maxOfAKindFound(int hand[])
+	
 	//this function returns the count of the die value occurring most in the hand
 	//but not the value itself
+	public static int maxOfAKindFound(int[] hand)
 	{
-	    int maxCount = 0;
+		int maxCount = 0;
 	    int currentCount ;
+	    
 	    for (int dieValue = 1; dieValue <=6; dieValue++)
 	    {
 	        currentCount = 0;
@@ -126,79 +138,94 @@ public class yahtzee
 	        if (currentCount > maxCount)
 	            maxCount = currentCount;
 	    }
+	    
 	    return maxCount;
 	}
-	int totalAllDice(int hand[])
-	//this function returns the total value of all dice in a hand
-	{
-	    int total = 0;
-	    for (int diePosition = 0; diePosition < 5; diePosition++)
-	    {
-	        total += hand[diePosition];
-	    }
-	    return total;
-	}
-	void sortArray(int array[], int size)
-	//bubble sort from  Gaddis chapter 8
-	{
-	   bool swap;
-	   int temp;
 	
-	   do
-	   {
-	      swap = false;
-	      for (int count = 0; count < (size - 1); count++)
-	      {
-	         if (array[count] > array[count + 1])
-	         {
-	            temp = array[count];
-	            array[count] = array[count + 1];
-	            array[count + 1] = temp;
-	            swap = true;
-	         }
-	      }
-	   } while (swap);
-	}
-	int maxStraightFound(int hand[])
 	//this function returns the length of the longest
 	//straight found in a hand
+	public static int maxStraightFound(int[] hand)
 	{
-	    int maxLength = 1;
-	    int curLength = 1;
-	    for(int counter = 0; counter < 4; counter++)
-	    {
-	        if (hand[counter] + 1 == hand[counter + 1] ) //jump of 1
-	            curLength++;
-	        else if (hand[counter] + 1 < hand[counter + 1]) //jump of >= 2
-	            curLength = 1;
-	        if (curLength > maxLength)
-	            maxLength = curLength;
-	    }
-	    return maxLength;
+		int maxLength = 1;
+		int curLength = 1;
+
+		for(int counter = 0; counter < 4; counter++)
+		{
+			if (hand[counter] + 1 == hand[counter + 1] ) //jump of 1
+				curLength++;
+			else if (hand[counter] + 1 < hand[counter + 1]) //jump of >= 2
+				curLength = 1;
+			if (curLength > maxLength)
+				maxLength = curLength;
+		}
+
+		return maxLength;
 	}
-	bool fullHouseFound(int hand[])
+	
 	//this function returns true if the hand is a full house
 	//or false if it does not
+	public static boolean fullHouseFound(int[] hand)
 	{
-	    bool foundFH = false;
-	    bool found3K = false;
-	    bool found2K = false;
-	    int currentCount ;
+		boolean foundFH = false;
+	    boolean found3K = false;
+	    boolean found2K = false;
+	    int currentCount;
+	    
 	    for (int dieValue = 1; dieValue <=6; dieValue++)
 	    {
 	        currentCount = 0;
+	        
 	        for (int diePosition = 0; diePosition < 5; diePosition++)
 	        {
 	            if (hand[diePosition] == dieValue)
 	                currentCount++;
 	        }
+	        
 	        if (currentCount == 2)
 	            found2K = true;
+	        
 	        if (currentCount == 3)
 	            found3K = true;
 	    }
+	    
 	    if (found2K && found3K)
 	        foundFH = true;
+	    
 	    return foundFH;
+	}
+	
+	//this function returns the total value of all dice in a hand
+	public static int totalAllDice(int[] hand)
+	{
+		int total = 0;
+		
+	    for (int diePosition = 0; diePosition < 5; diePosition++)
+	    {
+	        total += hand[diePosition];
+	    }
+	    
+	    return total;
+	}
+	
+	//bubble sort from Gaddis chapter 8
+	public static void sortArray(int[] hand, int size)
+	{
+		boolean swap;
+		int temp;
+
+		do
+		{
+			swap = false;
+			for (int count = 0; count < (size - 1); count++)
+			{
+				if (hand[count] > hand[count + 1])
+				{
+					temp = hand[count];
+					hand[count] = hand[count + 1];
+					hand[count + 1] = temp;
+					swap = true;
+				}
+			}
+		} while (swap);
 	}
 }
