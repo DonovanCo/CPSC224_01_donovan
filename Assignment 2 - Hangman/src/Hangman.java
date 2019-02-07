785c9201eeac86b21ea6d863d37d7dfdb1c32864
colet <colet@147.222.230.104>
2019-02-06 16:24:17 -0800

    Pushing main assignment folder.

---
+++ Assignment 2 - Hangman/src/Hangman.java
@@ -0,0 +1,18 @@
+
+/****************************************
+	Homework #2
+	Due Date: 02-11-2019
+	Names: Ian Gioffre and Cole Donovan
+****************************************/
+
+public class Hangman {
+
+	// main: serves only to call other functions
+	// to run the main hangman game.
+	public static void main(String[] args) {
+		
+		
+		
+	}
+
+}
