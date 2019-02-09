2c30020996d8aea1cc9e3adffa09d187ed7b0485
iangioffre <igioffre@zagmail.gonzaga.edu>
2019-02-09 13:13:14 -0800

    added some hangman strings and more menu functionality

--- Assignment 2 - Hangman/src/Hangman.java
+++ Assignment 2 - Hangman/src/Hangman.java
@@ -15,7 +15,7 @@ public class Hangman
 	// to run the main hangman game.
 	public static void main(String[] args)
 	{
-		
+		menu();
 	}
 	
 	public static String randomWord()
@@ -32,33 +32,88 @@ public class Hangman
 		
 		while(choice < 1 || choice > 3)
 		{
-			JOptionPane.showMessageDialog(null, "Welcome to Hangman!");
-			JOptionPane.showMessageDialog(null, "1. Play a game with a random word");
-			JOptionPane.showMessageDialog(null, "2. Play a game with a word from a friend");
-			JOptionPane.showMessageDialog(null, "3. Exit the game");
-			JOptionPane.showInputDialog("Enter the number of your choice");
+			input = JOptionPane.showInputDialog("Welcome to Hangman!\n"
+												+ "1. Play a game with a random word\n"
+												+ "2. Play a game with a word from another person\n"
+												+ "3. Exit the game\n"
+												+ "Enter the number of your choice");
+			choice = Integer.parseInt(input);
 			
 			switch(choice)
 			{
 				case 1: 
-					
+					Player game = new Player(randomWord());
+					playGame(game.getGuessWord());
+					break;
+				case 2:
+					Player game = new Player(getPlayerWord());
+					playGame(game.getGuessWord());
+					break;
+				case 3:
+					showMessageDialog(null, "Thanks for playing!");
+					System.exit(0);
+					break;
+				default:
+					showMessageDialog(null, "That is not an option. Try again.");
 			}
 		}
 	}
 	
-	public static char[] getPlayerWord()
+	public static String getPlayerWord()
 	{
-		
+		return JOptionPane.showInputDialog("Enter a word for another player to guess.");
 	}
 	
-	public static void showHangman()
+	public static String hangman(int strikes)
 	{
+		String output;
 		
+		switch(strikes)
+		{
+			case 0:
+				output = "      \n"
+					   + "      \n"
+					   + "      \n";
+				break;
+			case 1:
+				output = "  o   \n"
+					   + "      \n"
+					   + "      \n";
+				break;
+			case 2:
+				output = "  o   \n"
+			 	 	   + "  |   \n"
+			 	 	   + "      \n";
+				break;
+			case 3:
+				output = "  o   \n"
+				 	   + "/ |   \n"
+				 	   + "      \n";
+				break;
+			case 4:
+				output = "  o   \n"
+				 	   + "/ | \\\n"
+				 	   + "      \n";
+				break;
+			case 5:
+				output = "  o   \n"
+					   + "/ | \\\n"
+					   + " /    \n";
+				break;
+			case 6:
+				output = "  o   \n"
+				 	   + "/ | \\\n"
+				 	   + " / \\ \n";
+				break;
+		}
+		return output;
 	}
 	
 	public static void playGame(char[] word)
 	{
 		
+		
+		showHangman(game.getStrikes());
 	}
 
 }
