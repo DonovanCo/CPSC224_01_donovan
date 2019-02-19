916c36327c795f43278d0372959645ed096d42bc
colet <colet@147.222.226.166>
2019-02-19 15:56:59 -0800

    Began setting up the frame

--- Assignment 3 - TicTacToe/src/GameBoard.java
+++ Assignment 3 - TicTacToe/src/GameBoard.java
@@ -7,4 +7,19 @@ import java.awt.event.*;
 public class GameBoard extends JFrame
 {
 	
+	public GameBoard()
+	{
+		// Display a title.
+	    setTitle("TicTacToe");
+	    
+	    //sets the size of the frame.
+	    setSize(500, 500);
+
+	    // Specify an action for the close button.
+	    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+	    // Create a BorderLayout manager.
+	    setLayout(new BorderLayout());
+	}
+	
 }
