e995904e479d4d546f314a62ee84bcf9c9aa4b55
colet <colet@147.222.221.204>
2019-03-20 14:30:34 -0700

    Initial Commit

--- Assignment 4/src/Parallax.java
+++ Assignment 4/src/Parallax.java
@@ -1,4 +1,11 @@
 
+/*******************************************************************
+	Homework #4
+	Due Date: 03-25-2019
+	Names: Ian Gioffre and Cole Donovan
+	Repo URL: https://github.com/DonovanCo/CPSC224_01_donovan.git
+*******************************************************************/
+
 public class Parallax {
 
 }
