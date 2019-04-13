8448f3f71363c3493e24872fbbcadd7a930286dc
colet <colet@147.222.246.27>
2019-04-13 14:38:16 -0700

    Got Keyboard input for score working.

--- CPSC Project/src/GamePanel.java
+++ CPSC Project/src/GamePanel.java
@@ -14,7 +14,8 @@ public class GamePanel extends JPanel  implements ActionListener {
 	public GamePanel() {
 		timer = new Timer(delay, this);
 		timer.start();
-		//addKeyListener(new MyKeyListener());
+		addKeyListener(new MyKeyListener());
+		setFocusable(true);
 	}
 	
 	public void paint(Graphics g){
@@ -40,6 +41,38 @@ public class GamePanel extends JPanel  implements ActionListener {
 		repaint();
 	}
 	
+	class MyKeyListener extends KeyAdapter {
+		
+		@Override
+        public void keyReleased(KeyEvent e) {
+            resetSpeed();
+        }
+
+        @Override
+        public void keyPressed(KeyEvent e) {
+        	char ch = e.getKeyChar();
+        	if(ch=='a')
+        		goSlow();
+        	else if(ch=='d')
+        		goFast();
+        	else
+        		resetSpeed();
+        }
+	}
 	
+	public void goSlow() {
+		slow=true;
+		fast=false;
+	}
+	
+	public void goFast() {
+		slow=false;
+		fast=true;
+	}
+	
+	public void resetSpeed() {
+		slow=false;
+		fast=false;
+	}
 	
 }
