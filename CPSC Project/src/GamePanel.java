67dd26580bf57ec1c37c9ca4207d5bc12d0f2716
iangioffre <igioffre@zagmail.gonzaga.edu>
2019-04-13 14:52:33 -0700

    added sprites for standing and crouching

--- CPSC Project/src/GamePanel.java
+++ CPSC Project/src/GamePanel.java
@@ -2,7 +2,8 @@ import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 
-public class GamePanel extends JPanel  implements ActionListener {
+public class GamePanel extends JPanel implements ActionListener
+{
 	private boolean slow = false;
 	private boolean fast = false;
 	private int score = 0;
@@ -10,36 +11,36 @@ public class GamePanel extends JPanel  implements ActionListener {
 	private int count = 0;
 	private int baseScore = 2;
 	protected Timer timer;
-	
-	public GamePanel() {
+
+	public GamePanel()
+	{
 		timer = new Timer(delay, this);
 		timer.start();
-		//addKeyListener(new MyKeyListener());
+		// addKeyListener(new MyKeyListener());
 	}
-	
-	public void paint(Graphics g){
+
+	public void paint(Graphics g)
+	{
 		g.setColor(Color.white);
-		g.fillRect(0,0,600,600);
+		g.fillRect(0, 0, 600, 600);
 		g.setColor(Color.red);
-		g.setFont(new Font("SansSerif", Font.BOLD, 10)); 
+		g.setFont(new Font("SansSerif", Font.BOLD, 10));
 		g.drawString(Integer.toString(score), 10, 10);
 
 	}
-	
-	public void actionPerformed(ActionEvent e) {
+
+	public void actionPerformed(ActionEvent e)
+	{
 		count++;
-		if(count%delay==0)
+		if (count % delay == 0)
 		{
-			if(slow)
-				score+=baseScore/2;
-			else if(fast)
-				score+=baseScore*2;
+			if (slow)
+				score += baseScore / 2;
+			else if (fast)
+				score += baseScore * 2;
 			else
-				score+=baseScore;
+				score += baseScore;
 		}
 		repaint();
 	}
-	
-	
-	
 }
--- CPSC Project/src/GameRunner.java
+++ CPSC Project/src/GameRunner.java
@@ -2,25 +2,25 @@ import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 
-public class GameRunner extends JFrame{
-	
-	
+public class GameRunner extends JFrame
+{
+
 	GameRunner()
 	{
-		setTitle("Test");	
-		
+		setTitle("Test");
+
 		setSize(600, 600);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setResizable(false);	
-		
+		setResizable(false);
+
 		GamePanel tp = new GamePanel();
 		add(tp);
-		
+
 		setVisible(true);
 	}
-	
-	public static void main(String[] args){
+
+	public static void main(String[] args)
+	{
 		new GameRunner();
 	}
-	
 }
