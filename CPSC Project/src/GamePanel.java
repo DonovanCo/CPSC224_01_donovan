4c281535c79e2389e4cbbdee0830e57ef9d38af6
colet <colet@147.222.246.27>
2019-04-14 17:19:11 -0700

    Added jumping and basic collision detection

--- CPSC Project/src/GamePanel.java
+++ CPSC Project/src/GamePanel.java
@@ -6,19 +6,24 @@ public class GamePanel extends JPanel implements ActionListener
 {
 	private boolean slow = false;
 	private boolean fast = false;
+	private boolean isJumping = false;
 	private int score = 0;
 	private int delay = 25;
 	private int count = 0;
 	private int baseScore = 2;
 	private int dX = 2;
 	private int dY = 0;
-	private int playerY;
+	private int playerY = 150;
 	protected Timer timer;
 	private Image standingImg;
 	private Image crouchingImg;
-
+	
+	private int currLevel = 0;
 	private int mapX[] = { 0 };
 	private int mapY[] = { 450 };
+	private int mapWidth[] = {1000};
+	private int mapHeight[] = {150};
+	
 
 	public GamePanel()
 	{
@@ -37,22 +42,25 @@ public class GamePanel extends JPanel implements ActionListener
 		g.setColor(Color.white);
 		g.fillRect(0, 0, 600, 600);
 		g.setColor(Color.black);
-		g.fillRect(mapX[0], mapY[0], 1000, 150);
+		g.fillRect(mapX[0], mapY[0], mapWidth[0], mapHeight[0]);
 		g.setColor(Color.red);
 		g.setFont(new Font("SansSerif", Font.BOLD, 10));
 		g.drawString(Integer.toString(score), 10, 10);
+		
+		playerY+=dY;
+		
 		if(dY!=0)
 		{
-			g2d.drawImage(crouchingImg, 50, 300, this);
+			g2d.drawImage(crouchingImg, 50, playerY, this);
 		}
 		else
-			g2d.drawImage(standingImg, 50, 300, this);
+			g2d.drawImage(standingImg, 50, playerY, this);
 
 	}
 
 	public void actionPerformed(ActionEvent e)
 	{
-		count++;
+		
 		if (slow)
 		{
 			score += baseScore / 2;
@@ -66,6 +74,38 @@ public class GamePanel extends JPanel implements ActionListener
 			score += baseScore;
 			dX = baseScore;
 		}
+		
+		if(isJumping)
+		{
+			dY=-2;
+			count++;
+		}
+		if(count==35)
+		{
+			count = 0;
+			isJumping = false;
+		}
+		
+		if(!isJumping && playerY<mapY[currLevel]-62)
+		{
+			dY=2;
+		}
+		else if(mapX[currLevel]+mapWidth[currLevel]<=50)
+		{
+			dY=2;
+		}
+		else if(playerY>=mapY[currLevel]-62)
+		{
+			dY=0;
+			playerY=mapY[currLevel]-62;
+		}
+		
+		
+		
+		
+		
+		
+		
 		reMap();
 		repaint();
 	}
@@ -89,6 +129,20 @@ public class GamePanel extends JPanel implements ActionListener
 				goFast();
 			else
 				resetSpeed();
+			
+			if(ch == ' ')
+			{
+				jump();
+			}
+		}
+	}
+	
+	public void jump()
+	{
+		if(!isJumping&&dY==0)
+		{
+			isJumping=true;
+			playerY--;
 		}
 	}
 
